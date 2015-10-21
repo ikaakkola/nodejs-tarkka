c0e02dc91f16d6ecd6c014ba261e2c566478c3ef
xxxenic <xenic@saunalahti.fi>
2015-10-22 00:01:25 +0300

    Update tarkka_particleIo.js

--- js/tarkka_particleIo.js
+++ js/tarkka_particleIo.js
@@ -11,9 +11,20 @@
  * 
  * nodejs tarkka_particleIo.js /path/to/tarkka_query.js /tmp/tarkka_data.json highest 5 apikey eventname 
  * 
- * Copyright 2015, Ilkka Kaakkola <xenic@iki.fi>
  * 
- * Licensed under Apache License 2.0
+ *  Copyright 2015 Ilkka Kaakkola <xenic@iki.fi>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 // Usage information
