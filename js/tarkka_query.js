800d60dea2f19e2e895cc52e656f96a3c26a34f5
xxxenic <xenic@saunalahti.fi>
2015-10-22 00:02:08 +0300

    Update tarkka_query.js

--- js/tarkka_query.js
+++ js/tarkka_query.js
@@ -20,9 +20,20 @@
  * Query 5 highest hours, only returning hour 12 if it was within the 5 highest
  * nodejs tarkka_query.js /data/fetch_result.json highest 5 12
  * 
- * Copyright 2015, Ilkka Kaakkola <xenic@iki.fi>
  * 
- * Licensed under Apache License 2.0
+ *  Copyright 2015 Ilkka Kaakkola <xenic@iki.fi>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 //
