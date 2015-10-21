ff735d57fffd8d33db961bce6c1bc1e341038dde
Ilkka Kaakkola <xenic@basen.net>
2015-10-21 22:29:27 +0300

    examples in readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # nodejs-tarkka
+
 NodeJS utilities to automate things based on Fortum Tarkka hourly prices
+
+
+## Examples
+
+__Fetch 'Tarkka' hourly data__
+
+    nodejs /path/to/tarkka_fetch.js /tmp/tarkka_result.json
+
+__Query highest 5 hours from previously fetched data__
+
+    nodejs /path/to/tarkka_query.js /tmp/tarkka_result.json highest 5
+
+__Publish particle.io event if current hour is one of the highest 5__
+
+    nodejs /path/to/tarkka_particleIo.js /path/to/tarkka_query.js /tmp/tarkka_result.json highest 5 access_token device_id highest-hours 60
+
+The above publishes an event to particle.io for the deice ''device_id'' if the current hour (the hour the command is
+run) is within the 5 highest hours.
+
+The published event JSON looks like this (command ran at 22:26):
+
+    {
+      "name":"highest-hours",
+      "hour":22,
+      "value":3.21,
+      "durationsec": 1985
+    }
+
--- js/tarkka_query.js
+++ js/tarkka_query.js
@@ -17,6 +17,9 @@
  * Query hours below or at given limit
  * nodejs tarkka_query.js /data/fetch_result.json under 3.21
  * 
+ * Query 5 highest hours, only returning hour 12 if it was within the 5 highest
+ * nodejs tarkka_query.js /data/fetch_result.json highest 5 12
+ * 
  * Copyright 2015, Ilkka Kaakkola <xenic@iki.fi>
  * 
  * Licensed under Apache License 2.0
@@ -31,119 +34,190 @@ today.setHours( 0 );
 today.setMinutes( 0 );
 today.setSeconds( 0 );
 today.setMilliseconds( 0 );
-var resultFile = null;
-var mode = null;
-var count = 1;
-var limit = 5.00;
+
+//
+// We maybe used as a module
+//
+module.exports = {
+    /**
+     * Query the highest count hours from the file, returning a JSON object
+     * containing the result. If hour is specified, only returns value for that 
+     * hour, or no values if that hour is not one of the highest.
+     */
+    queryHighest: function( file, count, hour ) {
+        return doQuery( file, "highest", count, hour );
+    },
+    /**
+     * Query the lowest count hours from the file, returning a JSON object
+     * containing the result. If hour is specified, only returns value for that 
+     * hour, or no values if that hour is not one of the lowest.
+     */
+    queryLowest: function( file, count, hour ) {
+        return doQuery( file, "lowest", count, hour );
+    },
+    /**
+     * Query the values under or at given limit, returning a JSON object
+     * containing the result. If hour is specified, only returns value for that 
+     * hour, or no values if that hour has no value under the limit.
+     */
+    queryUnder: function( file, limit, hour ) {
+        return doQuery( file, "under", limit, hour );
+    },
+    /**
+     * Query the values over or at given limit, returning a JSON object
+     * containing the result. If hour is specified, only returns value for that 
+     * hour, or no values if that hour has no value over the limit.
+     */
+    queryOver: function( file, limit, hour ) {
+        return doQuery( file, "over", limit, hour );
+    }
+}
 
 // Usage information
 var usage = function() {
-    console.log( "Usage: nodejs tarkka_query.js file mode [mode_specific]" );
+    console.log( "Usage: nodejs tarkka_query.js file mode [mode_specific hour]" );
     console.log( "\nfile - the full filesystem path to read data from." );
     console.log( "mode - mode of query, one of 'highest,lowest,under,over'" );
     console.log( "mode_specific - mode specific parameter, for highest and "
         + "lowest this is the number of values to return, for over and under this is the value limit to query for" );
+    console.log( "hour - only return value for this hour, 0 to 23. Returns empty result if there was no matching value for hour." );
     process.exit( 1 );
+}
+
+//
+// Utility functions
+//
+
+var makeError = function( text ) {
+    return JSON.stringify( {
+            "error": text,
+            "values": []
+        } );
+}
+
+var doQuery = function( file, mode, modeSpecific, hour ) {
+    // Look for result file
+    var valid = false;
+    try {
+        var fileStats = fs.statSync( file );
+        if( !fileStats.isFile() ) {
+            throw( "'" + file + "' is not a file." );
+        }
+
+        var data = JSON.parse( fs.readFileSync( file, 'utf8' ) );
+        if( data.time < today.getTime() ) {
+            throw( "File '" + file + "' does not contain valid results for today." );
+        }
+        var values = [];
+        for( var i = 0; i < data.data.length; i++ ) {
+            var val = data.data[ i ];
+            values.push( {
+                    "hour": i,
+                    "value": data.data[ i ]
+                } );
+        }
+
+        values.sort( function( a, b ) {
+                switch( mode.toLowerCase() ) {
+                    case "highest":
+                    case "over":
+                        if( a.value > b.value ) {
+                            return -1;
+                        }
+                        if( b.value > a.value ) {
+                            return 1;
+                        }
+                        return a.hour > b.hour;
+                    case "lowest":
+                    case "under":
+                        if( a.value > b.value ) {
+                            return 1;
+                        }
+                        if( b.value > a.value ) {
+                            return -1;
+                        }
+                        return a.hour > b.hour;
+                }
+            } );
+        var resultValues;
+        switch( mode.toLowerCase() ) {
+            case "highest":
+            case "lowest":
+                var count = modeSpecific == null ? 5: parseInt( modeSpecific );
+                if( count <= 0 ) {
+                    count = 1;
+                }
+                if( count >= values.length ) {
+                    count = values.length;
+                }
+                resultValues = values.splice( 0, count );
+                break;
+            case "over":
+                var limit = modeSpecific == null ? 6.00: parseFloat( modeSpecific );
+                resultValues = [];
+                for( var i = 0; i < values.length; i++ ) {
+                    if( values[ i ].value >= limit ) {
+                        resultValues.push( values[ i ] );
+                    }
+                }
+                break;
+            case "under":
+                var limit = modeSpecific == null ? 6.00: parseFloat( modeSpecific );
+                resultValues = [];
+                for( var i = 0; i < values.length; i++ ) {
+                    if( values[ i ].value <= limit ) {
+                        resultValues.push( values[ i ] );
+                    }
+                }
+        }
+
+        if( hour >= 0 && hour < 24 ) {
+            var want = null;
+            for( var i = 0; i < resultValues.length; i++ ) {
+                if( resultValues[ i ].hour == hour ) {
+                    want = resultValues[ i ];
+                    break;
+                }
+            }
+            resultValues = [];
+            if( want != null ) {
+                resultValues.push( want );
+            }
+        }
+        var res = {
+            "values": resultValues,
+            "unit": "c/kWh"
+        }
+        return res;
+    } catch( e ) {
+        throw( "Unable to read data from '" + resultFile + "': " + e );
+    }
 }
 
 //
 // Main
 //
+var runningAsScript = !module.parent;
 
-// Process arguments
-var args = process.argv.splice( 2 );
-if( args.length < 2 ) {
-    usage();
-}
+if( runningAsScript ) {
 
-resultFile = args[ 0 ];
-mode = args[ 1 ];
-if( "highest" === mode.toLowerCase() || "lowest" === mode.toLowerCase() ) {
-    count = args.length > 2 ? args[ 2 ]: count;
-} else {
-    limit = args.length > 2 ? args[ 2 ]: count;
-}
+    // Process arguments
+    var args = process.argv.splice( 2 );
+    if( args.length < 2 ) {
+        usage();
+    }
 
-// Look for result file
-var valid = false;
-try {
-    var fileStats = fs.statSync( resultFile );
-    if( !fileStats.isFile() ) {
-        console.log( "'" + resultFile + "' is not a file." );
+    var resultFile = args[ 0 ];
+    var mode = args[ 1 ];
+    var modeParam = args.length > 2 ? args[ 2 ]: null;
+    var hour = args.length > 3 ? args[ 3 ]: hour;
+
+    try {
+        var res = doQuery( resultFile, mode.toLowerCase(), modeParam, hour );
+        console.log( JSON.stringify( res ) );
+        process.exit( 0 );
+    } catch( e ) {
+        console.log( makeError( "" + e ) );
         process.exit( 1 );
     }
-
-    var data = JSON.parse( fs.readFileSync( resultFile, 'utf8' ) );
-    if( data.time < today.getTime() ) {
-        console.log( "File '" + resultFile + "' does not contain valid results for today." );
-        process.exit( 1 );
-    }
-    var values = [];
-    for( var i = 0; i < data.data.length; i++ ) {
-        var val = data.data[ i ];
-        values.push( {
-                "hour": i,
-                "value": data.data[ i ]
-            } );
-    }
-
-    values.sort( function( a, b ) {
-            switch( mode.toLowerCase() ) {
-                case "highest":
-                case "over":
-                    if( a.value > b.value ) {
-                        return -1;
-                    }
-                    if( b.value > a.value ) {
-                        return 1;
-                    }
-                    return a.hour > b.hour;
-                case "lowest":
-                case "under":
-                    if( a.value > b.value ) {
-                        return 1;
-                    }
-                    if( b.value > a.value ) {
-                        return -1;
-                    }
-                    return a.hour > b.hour;
-            }
-        } );
-    var resultValues;
-    switch( mode.toLowerCase() ) {
-        case "highest":
-        case "lowest":
-            if( count <= 0 ) {
-                count = 1;
-            }
-            if( count >= values.length ) {
-                count = values.length;
-            }
-            resultValues = values.splice( 0, count );
-            break;
-        case "over":
-            resultValues = [];
-            for( var i = 0; i < values.length; i++ ) {
-                if( values[ i ].value >= limit ) {
-                    resultValues.push( values[ i ] );
-                }
-            }
-            break;
-        case "under":
-            resultValues = [];
-            for( var i = 0; i < values.length; i++ ) {
-                if( values[ i ].value <= limit ) {
-                    resultValues.push( values[ i ] );
-                }
-            }
-    }
-    var res = {
-        "values": resultValues,
-        "unit": "c/kWh"
-    }
-    console.log( JSON.stringify( res ) );
-    process.exit( 0 );
-} catch( e ) {
-    console.log( "Unable to read data from '" + resultFile + "': " + e );
-    process.exit( 1 );
 }
