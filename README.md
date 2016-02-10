537007c338e7ef6307c27b063a1ac25d981d399c
Ilkka Kaakkola <xenic@basen.net>
2016-02-10 10:11:39 +0200

    Updated readme

--- README.md
+++ README.md
@@ -15,8 +15,16 @@ __Query highest 5 hours from previously fetched data__
 
 __Publish particle.io event if current hour is one of the highest 5__
 
-    nodejs /path/to/tarkka_particleIo.js /path/to/tarkka_query.js /tmp/tarkka_result.json highest 5 access_token event_name 3600
+    nodejs /path/to/tarkka_particleIoEvent.js access_token event_name /path/to/tarkka_query.js /tmp/tarkka_result.json highest 5 
 
 The above publishes an event to particle.io as ''event_name'' if the current hour (the hour the command is
 run) is within the 5 highest hours.
+
+__Call 'myFunction' on device 'aaabbb' with parameter 'R0:HIGH' if current hour is over 4.2__
+
+    nodejs /path/to/tarkka_particleIoCallFunction.js access_token device_id myFunction R0:HIGH /path/to/tarkka_query.js /tmp/tarkka_result.json over 4.2
+
+
+
+
 
--- js/tarkka_query.js
+++ js/tarkka_query.js
@@ -20,7 +20,12 @@
  * Query 5 highest hours, only returning hour 12 if it was within the 5 highest
  * nodejs tarkka_query.js /data/fetch_result.json highest 5 12
  * 
+ * Query current hour, returning the value if it is over the given limit
+ * nodejs tarkka_query.js /data/fetch_result.json over 5.42 current
  * 
+ * Query current hour, returning the value if it is under the given limit
+ * nodejs tarkka_query.js /data/fetch_result.json under 3.42 current
+ *
  *  Copyright 2015 Ilkka Kaakkola <xenic@iki.fi>
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -53,7 +58,9 @@ module.exports = {
     /**
      * Query the highest count hours from the file, returning a JSON object
      * containing the result. If hour is specified, only returns value for that 
-     * hour, or no values if that hour is not one of the highest.
+     * hour, or no values if that hour is not one of the highest. 
+     *
+     * 'current' may be used as the hour value to use current hour.
      */
     queryHighest: function( file, count, hour ) {
         return doQuery( file, "highest", count, hour );
@@ -62,6 +69,8 @@ module.exports = {
      * Query the lowest count hours from the file, returning a JSON object
      * containing the result. If hour is specified, only returns value for that 
      * hour, or no values if that hour is not one of the lowest.
+     *
+     * 'current' may be used as the hour value to use current hour.
      */
     queryLowest: function( file, count, hour ) {
         return doQuery( file, "lowest", count, hour );
@@ -70,6 +79,8 @@ module.exports = {
      * Query the values under or at given limit, returning a JSON object
      * containing the result. If hour is specified, only returns value for that 
      * hour, or no values if that hour has no value under the limit.
+     *
+     * 'current' may be used as the hour value to use current hour.
      */
     queryUnder: function( file, limit, hour ) {
         return doQuery( file, "under", limit, hour );
@@ -78,6 +89,8 @@ module.exports = {
      * Query the values over or at given limit, returning a JSON object
      * containing the result. If hour is specified, only returns value for that 
      * hour, or no values if that hour has no value over the limit.
+     *
+     * 'current' may be used as the hour value to use current hour.
      */
     queryOver: function( file, limit, hour ) {
         return doQuery( file, "over", limit, hour );
@@ -91,7 +104,7 @@ var usage = function() {
     console.log( "mode - mode of query, one of 'highest,lowest,under,over'" );
     console.log( "mode_specific - mode specific parameter, for highest and "
         + "lowest this is the number of values to return, for over and under this is the value limit to query for" );
-    console.log( "hour - only return value for this hour, 0 to 23. Returns empty result if there was no matching value for hour." );
+    console.log( "hour - only return value for this hour, 0 to 23. Returns empty result if there was no matching value for hour. Use 'current' for current hour." );
     process.exit( 1 );
 }
 
@@ -107,6 +120,10 @@ var makeError = function( text ) {
 }
 
 var doQuery = function( file, mode, modeSpecific, hour ) {
+    if( typeof hour !== "undefined" && "current" === hour ) {
+	hour = new Date().getHours();
+    }
+
     // Look for result file
     var valid = false;
     try {
@@ -119,6 +136,7 @@ var doQuery = function( file, mode, modeSpecific, hour ) {
         if( data.time < today.getTime() ) {
             throw( "File '" + file + "' does not contain valid results for today." );
         }
+
         var values = [];
         for( var i = 0; i < data.data.length; i++ ) {
             var val = data.data[ i ];
